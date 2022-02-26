8dd559c41044cacfa2d40adf5cf3e3aab5e94ca1
manrique-code <sergio.mnrq1@gmail.com>
2022-02-26 16:35:17 -0600

    Implementado nuevas rutas * Implementada nueva ruta para crear el usuario del docente

--- routes/api/v1/docentes/docentes.js
+++ routes/api/v1/docentes/docentes.js
@@ -43,4 +43,22 @@ router.post("/new", async (req, res) => {
   }
 }); // post: /new
 
+// Ruta para crear el usuario de un docente /signin
+router.put("/signin/:identidad", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const { identidad } = req.params;
+    const result = await docenteModel.newUsuarioDocente(
+      identidad,
+      email,
+      password
+    );
+    res.status(200).json({ status: "success", result });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ status: "failed" });
+  }
+});
+// post: /sigin
+
 module.exports = router;
