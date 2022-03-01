330be831f718910f386019c03aaf68e945a24422
Hader-Mendez <hader.mendez@gmail.com>
2022-02-28 23:18:15 -0600

    Creacion del estudiante, usuario y encargado

--- routes/api/v1/v1.js
+++ routes/api/v1/v1.js
@@ -4,7 +4,9 @@ const router = express.Router();
 // Rutas de los controladores de cada clase necesitados por el API.
 // Enrutador de Docentes.
 const docentesRoutes = require("./docentes/docentes");
+const estudiantesRoutes = require("./estudiantes/estudiantes");
 
 router.use("/docentes", docentesRoutes);
+router.use("/estudiantes", estudiantesRoutes);
 
 module.exports = router;
