2cfc4a3f5126ef43814b01541f506641a9027bc7
manrique-code <sergio.mnrq1@gmail.com>
2022-03-16 00:29:58 -0600

    Limpieza y refactorización de código

--- routes/api/v1/tareas/tareas.js
+++ routes/api/v1/tareas/tareas.js
@@ -28,6 +28,7 @@ router.put("/newtarea/:id", async (req, res) => {
   try {
     const { fechaAsignada, fechaEntrega, estado, info } = req.body;
     const { id } = req.params;
+
     const rslt = await tareasModel.newTarea(
       id,
       fechaAsignada,
@@ -46,6 +47,7 @@ router.put("/updateTarea/:id", async (req, res) => {
   try {
     const { fechaAsignada, fechaEntrega, estado, info, numeroTarea } = req.body;
     const { id } = req.params;
+
     const rslt = await tareasModel.updateTareaAsig(
       id,
       fechaAsignada,
@@ -55,6 +57,95 @@ router.put("/updateTarea/:id", async (req, res) => {
       numeroTarea
     );
     res.status(200).json({ status: "ok", rslt });
+  } catch (ex) {
+    console.log(ex);
+    res.status(500).json({ status: "failed" });
+  }
+});
+
+router.put("/newentregable/:iddoc/:idest", async (req, res) => {
+  try {
+    const { iddoc, idest } = req.params;
+    const { numeroTarea, documentoURL, estado } = req.body;
+    const tiempoTranscurrido = Date.now();
+    const hoy = new Date(tiempoTranscurrido);
+
+    const fechaEntrega =
+      hoy.getDate() +
+      "-" +
+      (hoy.getMonth() + 1) +
+      "-" +
+      hoy.getFullYear() +
+      " " +
+      hoy.getHours() +
+      ":" +
+      hoy.getMinutes() +
+      ":" +
+      hoy.getSeconds() +
+      ":" +
+      hoy.getMilliseconds();
+
+    const rslt = await entregablesModel.newEntrega(
+      iddoc,
+      idest,
+      numeroTarea,
+      fechaEntrega,
+      documentoURL,
+      estado
+    );
+    res.status(200).json({ status: "ok", rslt });
+  } catch (ex) {
+    console.log(ex);
+    res.status(500).json({ status: "failed" });
+  }
+});
+
+router.put("/updateentregable/:iddoc/:idest", async (req, res) => {
+  try {
+    const { iddoc, idest } = req.params;
+    const { numeroTarea, documentoURL, estado } = req.body;
+    const tiempoTranscurrido = Date.now();
+    const hoy = new Date(tiempoTranscurrido);
+
+    const fechaEntrega =
+      hoy.getDate() +
+      "-" +
+      (hoy.getMonth() + 1) +
+      "-" +
+      hoy.getFullYear() +
+      " " +
+      hoy.getHours() +
+      ":" +
+      hoy.getMinutes() +
+      ":" +
+      hoy.getSeconds() +
+      ":" +
+      hoy.getMilliseconds();
+
+    const rslt = await entregablesModel.updateEntrega(
+      iddoc,
+      idest,
+      numeroTarea,
+      fechaEntrega,
+      documentoURL,
+      estado
+    );
+    res.status(200).json({ status: "ok", rslt });
+  } catch (ex) {
+    console.log(ex);
+    res.status(500).json({ status: "failed" });
+  }
+});
+
+//router delete tarea()
+router.delete("/deleteTarea/:id/:idtarea", async (req, res) => {
+  try {
+    const { id, idtarea } = req.params;
+    const result = await tareasModel.deleteTarea(id, idtarea);
+    res.status(200).json({
+      status: "ok",
+      result,
+    });
   } catch (ex) {
     console.log(ex);
     res.status(500).json({ status: "failed" });
