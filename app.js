f4022c71dae83844648e75bc66d4f47cf60e54fa
EmilPaizano <pabloemilio.beta@gmail.com>
2022-03-30 00:47:32 -0600

    Implementacion de CORS

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var cors = require('cors')
 
 /**
  * La razón de este require es porque tenía problemas al momento de utilizar la librería.
@@ -9,12 +10,33 @@ var logger = require("morgan");
  */
 require("dotenv").config();
 
+/**
+ * Implementacion de CORS
+ */
+ 
+ //extrae de las variables de entorno a CORS_ORIGIN que trae
+ //consigo las URL que estaran en la whitelist
+ var whiteList = (process.env.CORS_ORIGIN || '').split(',')
+ console.log(whiteList)
+ var corsOptions = {
+     origin: (origin,callback)=>{
+         console.log("Origin value: ",origin)
+         if(whiteList.indexOf(origin) >= 0 ){
+             callback(null,true)
+         }else{
+             callback(new Error('CORS not Allowed'))
+         }
+     }
+ }
+
+
 var indexRouter = require("./routes/index");
 var apiRouter = require("./routes/api/api");
 
 var app = express();
 
 app.use(logger("dev"));
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
